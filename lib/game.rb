02f76d167859a8b8ef106c1d7f98dbbd50c6a525
Sean Furlong <seanworkcomputer@Seans-MacBook-Pro.local>
2022-05-16 16:17:39 -0400

    display the game board

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
-class Game
+# this is the baord for the tic tac toe game
+class Board
+  attr_accessor :value_board, :new_board
+
+  # rubocop:disable Metrics/AbcSize
+  def initialize
+    # rubocop:enable Metrics/AbcSize
+    @value_board = %w[1 2 3 4 5 6 7 8 9]
+    # rubocop:disable Layout/LineLength
+    @new_board = " #{value_board[0]} | #{value_board[1]} | #{value_board[2]}\n___|___|___\n #{value_board[3]} | #{value_board[4]} | #{value_board[5]}\n___|___|___\n #{value_board[6]} | #{value_board[7]} | #{value_board[8]}"
+    # rubocop:enable Layout/LineLength
+  end
+end
+
+# this allows the game to be launched
+class Game < Board
   puts 'Welcome to TTT'
+  def new_game
+    start = Board.new
+    puts start.new_board
+  end
 end
